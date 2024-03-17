1232e822334b5d8d18794f9529d8876e1f2d6785
Barbara Kulig <basia1316.bk@gmail.com>
2024-03-17 09:09:21 +0100

    zadanie 2

---
+++ DOCKER.py
@@ -0,0 +1,23 @@
+# Użyj obrazu bazowego Pythona
+FROM python:3.9-slim
+
+# Ustaw katalog roboczy na /app
+WORKDIR /app
+
+# Skopiuj plik wymagany dla Asystenta Osobistego do katalogu roboczego
+COPY requirements.txt .
+
+# Zainstaluj wymagane pakiety
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Skopiuj wszystkie pliki źródłowe do katalogu roboczego
+COPY . .
+
+# Uruchom Asystenta Osobistego
+CMD ["python", "assistant.py"]
+
+#Budowanie obrazu Dockerowego
+docker build -t personal_assistant .
+
+# Możliwość utworzenia nowego kontenera
+docker run -d --name personal_assistant_container personal_assistant
--- zadanie 2..py
+++ zadanie 2..py
@@ -8,21 +8,13 @@ pipenv --python 3.10
 # Otwórz plik Pipfile i w terminalu wpisz
 pip install
 
-
 # Pracuję na PyCharmie
-
 # Aby skonfigurować IDE:
-
 # 1. Otwórz projekt w PyCharm.
-
 # 2. Przejdź do menu "File" -> "Settings"
-
 # 3. W ustawieniach, przejdź do sekcji "Project: Kurs" -> "Python Interpreter".
-
 # 4. Kliknij ikonę koła zębatego obok rozwijalnej listy interpreterów Pythona i wybierz "Add..." (Dodaj).
-
 # 5. Wybierz opcję "Pipenv Environment" z listy dostępnych opcji.
-
 # 6. W nowym oknie dialogowym, wybierz istniejące wirtualne środowisko.
 
 # 7. Po wybraniu odpowiedniego środowiska, kliknij "Ok" aby zatwierdzić.

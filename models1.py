34a447a778eea604e5a7f1ea3004e8d4fc7d6337
Barbara Kulig <basia1316.bk@gmail.com>
2024-06-12 22:32:44 +0200

    zadanie 9

--- models1.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, BooleanField
-
-
-class Contact(Document):
-    first_name = StringField(required=True)
-    last_name = StringField(required=True)
-    email = StringField(required=True)
-    email_sent = BooleanField(default=False)
